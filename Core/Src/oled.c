4bb3bdbe357edf47cc9396296bf1da0f1e5d41ea
Ferchisx <fefernandeza@unal.edu.co>
2024-09-13 16:36:30 -0500

    Comparation function changed
    I changed the function to compare IDs to make it look better and finally make it work as it should be

--- Core/Src/oled.c
+++ Core/Src/oled.c
@@ -5,45 +5,24 @@
  *      Author: Felipe
  */
 #include <main.h>
-#include <oled.h>
-#include <sys_stat.h>
+#include <stdio.h>
+#include <control.h>
 
-char num[2];		/*Control de la tecla ingresada individual*/
-char input_id[11];	/*Variable para guardar todo el ID*/
-int digit = 0;		/*Variable de control para guardar los números*/
-uint8_t state;
-
-uint8_t comparation(void){
-	char my_id[] = "1056120378";
-	ssd1306_Reset();
-	if(strcmp(input_id, my_id)==0){
-		ssd1306_WriteString("Success", Font_7x10, White);
-		return 1;
-	}else{
-		ssd1306_WriteString("Error", Font_7x10, White);
-		return 0;
-	}
-}
-
-uint8_t digits(uint8_t key_pressed){
-	if((key_pressed >= '0')&&(key_pressed <= '9')){
-		if(digit<10){
-			input_id[digit] = key_pressed;
-			digit++;
-			input_id[digit] = '\0';
-		}
-	}
-	if(key_pressed == '*'){
-		input_id[0] = '\0';
-		digit = 0;
-		ssd1306_Reset();
-	}
-	if(key_pressed == '#'){
-		uint8_t state = comparation();
-	}
-	num[0] = key_pressed;
-	num[1] = '\0';
-	ssd1306_WriteString(num, Font_7x10, White);
-	ssd1306_UpdateScreen();
-	return state;
-}
+//void comparation(void){
+//	char my_id[] = "1056120378";
+//	ssd1306_Fill(Black);
+//	ssd1306_UpdateScreen();
+//	int8_t id_incorrect = 0;
+//	for (uint8_t idx = 0; idx < sizeof(my_id); idx++) {
+//		if(my_id[idx]!=input_id[idx]) {
+//			id_incorrect = 1;
+//		}
+//	}
+//	if (id_incorrect == 0) {
+//		ssd1306_WriteString("Success", Font_7x10, White);
+//		ssd1306_UpdateScreen();
+//	}else{
+//		ssd1306_WriteString("Error", Font_7x10, White);
+//		ssd1306_UpdateScreen();
+//	}
+//}
